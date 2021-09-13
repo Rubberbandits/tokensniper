f90c7dcbc15568158d9c1970de0758256208d81b
rusty <dalegribble@riseup.net>
2021-09-13 10:56:50 -0700

    begin dependencies

--- server/network.js
+++ server/network.js
@@ -1,98 +1,95 @@
 const console = require("console");
-const http = require("http");
-const https = require("https");
+
 
 var PROXY_AGENTS = {};
 var PROXIES = [
 	{
 		host: "154.16.39.77",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
-	/*
 	{	// dead?
 		host: "181.215.191.255",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
-	*/
 	{
 		host: "102.129.240.162",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "191.101.148.55",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "64.137.65.115",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "134.202.123.153",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "50.114.85.48",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "94.124.160.32",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "185.33.85.232",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "79.141.161.119",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "181.214.223.134",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "191.96.58.204",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "185.170.56.67",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "154.127.48.24",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
 	{
 		host: "64.188.7.161",
-		port: 45785,
+		port: 45786,
 		user: "Seldorku1337",
 		password: "F2b5HkZ",
 	},
@@ -119,29 +116,7 @@ function GET_PROXY_AGENT(_proxyID, _url)
 		if (!_proxyAgent && !_agentProcessing) {
 			PROXY_PROCESSING[_proxyID] = true;
 
-			http.request({
-				host: _proxyInfo.host, // IP address of proxy server
-				port: _proxyInfo.port, // port of proxy server
-				method: 'CONNECT',
-				path: _url.hostname + ":" + (_url.protocol == "https:" ? 443 : 80),
-				headers: {
-					'Proxy-Authorization': 'Basic ' + Buffer.from(_proxyInfo.user + ':' + _proxyInfo.password).toString('base64')
-				},
-			}).on('connect', (res, socket) => {
-				if (res.statusCode === 200) { // connected to proxy server
-					const agent = new https.Agent({ socket });
-
-					PROXY_AGENTS[_proxyID] = agent;
-					PROXY_PROCESSING[_proxyID] = false;
-					resolve(agent);
-				} else {
-					reject("Connection failed to proxy, Status Code: " + res.statusCode);
-				}
-			})
-			.on('error', _err => {
-				reject("Connection failed to proxy: " + _err);
-			})
-			.end();
+			
 		} else if (!_agentProcessing) {
 			resolve(_proxyAgent);
 		}
@@ -158,22 +133,7 @@ async function API_REQUEST(_tokenURI, _agentID) {
 	let _promise = new Promise((resolve, reject) => {
 		GET_PROXY_AGENT(_agentID, _tokenURI)
 			.then(_agent => {
-				https.get(
-					{
-						host: _tokenURI.hostname,
-						path: _tokenURI.pathname,
-						//agent: _agent,      // cannot use a default agent
-					}, (res) => {
-						let chunks = []
-						res.on('data', chunk => chunks.push(chunk))
-						res.on('end', () => {
-							resolve(Buffer.concat(chunks).toString('utf8'));
-						})
-					}
-				).on('error', _err => {
-					reject(_err);
-				})
-				.end();
+				
 			})
 			.catch(_err => {
 				reject(_err);

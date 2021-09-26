b588374f8e37b184a1f5b9143f58cfcec6293c20
rusty <dalegribble@riseup.net>
2021-09-25 17:00:15 -0700

    remove purchase api remove session api add JWT_SECRET to .env.example

--- client/pages/api/purchase.js
+++
@@ -1,7 +0,0 @@
-import ReactDOMServer from "react-dom/server";
-import DefaultErrorPage from 'next/error';
-
-export default function logout(req, res) 
-{
-	
-}
--- client/pages/api/session.js
+++
@@ -1,8 +0,0 @@
-import ReactDOMServer from "react-dom/server";
-import DefaultErrorPage from "next/error";
-import { db } from "../../lib/database";
-
-export default function logout(req, res) 
-{
-	res.status(200).send("test")
-}

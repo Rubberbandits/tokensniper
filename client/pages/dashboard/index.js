99feed4e5994201d7524ef69b34aedb4661ed1e2
rusty <dalegribble@riseup.net>
2021-09-30 17:53:58 -0700

    remove debug print

--- client/pages/dashboard/index.js
+++ client/pages/dashboard/index.js
@@ -56,7 +56,6 @@ export async function getServerSideProps(context) {
 					}
 				}
 
-				console.log(payload.iat)
 				const { sessionJWT, refreshJWT } = await RefreshSession(publicAddress, "10m", "24h", payload.iat);
 
 				res.setHeader('Set-Cookie', ['sessionJWT=' + sessionJWT, 'refreshJWT=' + refreshJWT]);

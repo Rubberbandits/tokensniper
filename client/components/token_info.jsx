842fa263a587caea1882a8677fc2f2a3c3a47819
rusty <dalegribble@riseup.net>
2021-10-30 16:54:01 -0700

    fix TokenInfo component to follow proper react methodology

--- client/components/token_info.jsx
+++ client/components/token_info.jsx
@@ -1,28 +1,39 @@
+import React from 'react';
 import DeepTokenInfo from "./deep_token_info";
 
-function OpenTokenDrawer(entry)
-{
-	document.getElementById(entry + "_drawer").classList.toggle("hidden");
-}
+export default class TokenInfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {open: false};
+	}
 
-export default function TokenInfo({entry, tokenData})
-{
-	return (
-		<>
-			<tr onClick={() => {OpenTokenDrawer(entry)}}>
-				<td>#{entry + 1}</td>
-				<td>{tokenData.collection.name}</td>
-				<td>{tokenData.tokenId}</td>
-				<td>{tokenData.name}</td>
-			</tr>
+	onClick() {
+		this.setState((state) => ({
+			open: !state.open
+		}));
+	}
 
-			<tr id={entry + "_drawer"} className="hidden">
-				<td>
-					<div className="overflow-visible flex w-0 ml-12">
-						<DeepTokenInfo tokenData={tokenData} />
-					</div>
-				</td>
-			</tr>
-		</>
-	)
+	render() {
+		let entry = this.props.entry;
+		let tokenData = this.props.tokenData;
+
+		return (
+			<>
+				<tr onClick={() => {this.onClick()}}>
+					<td>#{entry + 1}</td>
+					<td>{tokenData.collection.name}</td>
+					<td>{tokenData.tokenId}</td>
+					<td>{tokenData.name}</td>
+				</tr>
+	
+				{this.state.open && <tr>
+					<td>
+						<div className="overflow-visible flex w-0 ml-12">
+							<DeepTokenInfo tokenData={tokenData} />
+						</div>
+					</td>
+				</tr> || null}
+			</>
+		)
+	}
 }

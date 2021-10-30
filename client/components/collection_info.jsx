0f54668266349611e39743b23391bb06275610c9
rusty <dalegribble@riseup.net>
2021-10-30 12:46:05 -0700

    add search bar to wallet tracker

--- client/components/collection_info.jsx
+++ client/components/collection_info.jsx
@@ -30,23 +30,35 @@ export default function CollectionInfo({walletAddr})
 
 	console.log(info)
 	return (
-		<table data-theme="light" className="table w-full table-compact text-onyx">
-			<thead>
-				<tr>
-					<th></th> 
-					<th>Contract</th>
-					<th>Token ID</th>
-					<th>Name</th>
-					<th>Previous Floor</th>
-					<th>Current Floor</th>
-					<th>Percentage Delta</th>
-					<th>Trait Floor</th>
-				</tr>
-			</thead>
+		<>
+			<div data-theme="light" className="form-control w-96 m-2">
+				<label className="label">
+					<span className="label-text">Wallet Address</span>
+				</label> 
+				<div className="flex space-x-2">
+					<input id="walletAddr" type="text" placeholder="Search" className="w-full input" /> 
+					<button className="btn">go</button>
+				</div>
+			</div>
 
-			<tbody>
-				{[...entries]}
-			</tbody> 
-		</table>
+			<table data-theme="light" className="table w-full table-compact text-onyx">
+				<thead>
+					<tr>
+						<th></th> 
+						<th>Contract</th>
+						<th>Token ID</th>
+						<th>Name</th>
+						<th>Previous Floor</th>
+						<th>Current Floor</th>
+						<th>Percentage Delta</th>
+						<th>Trait Floor</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					{[...entries]}
+				</tbody> 
+			</table>
+		</>
 	)
 }

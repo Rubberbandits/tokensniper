b6b3db78c636d9462f9f5c1b5a8cd565f1d08692
rusty <dalegribble@riseup.net>
2021-10-30 12:02:29 -0700

    add client/api/dashboard/getWalletCollection endpoint

---
+++ client/pages/api/dashboard/getWalletCollection.js
@@ -0,0 +1,13 @@
+import { genericGetServerSideProps } from "../../../lib/verify_session";
+
+export var getServerSideProps = genericGetServerSideProps;
+
+export default async function getWalletCollection(req, res) 
+{
+	const {query: {publicAddress}, method} = req;
+
+	const response = await fetch("http://localhost:5000/api/scrape")
+		.then(res => res.json())
+	
+	res.status(200).send(response.data.query.search);
+}

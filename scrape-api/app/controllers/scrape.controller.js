e36a44150323568b4fdb339bf8c02a22405f7abb
rusty <dalegribble@riseup.net>
2021-10-29 17:30:26 -0700

    add gqlRun function to API

--- scrape-api/app/controllers/scrape.controller.js
+++ scrape-api/app/controllers/scrape.controller.js
@@ -1,7 +1,12 @@
 const scrape = require('../../config/nightmare');
 
 exports.test = function(req, res) {
+	scrape.gqlRun("collectionQuery", JSON.parse("{\"collection\":\"lady-killaz\",\"collections\":[\"lady-killaz\"],\"collectionQuery\":null,\"includeHiddenCollections\":null,\"numericTraits\":null,\"query\":null,\"sortAscending\":true,\"sortBy\":\"PRICE\",\"stringTraits\":null,\"toggles\":null,\"showContextMenu\":true,\"isCategory\":false,\"categories\":null,\"chains\":null,\"collectionSortBy\":\"SEVEN_DAY_VOLUME\",\"eventTypes\":[\"AUCTION_SUCCESSFUL\"],\"isSingleCollection\":true}"), (data) => {
+		res.status(200).send(data);
+	});
+
+	/*
     scrape.scrapeTest(function(data) {
         res.status(200).send(data);
-    })
+    })*/
 }

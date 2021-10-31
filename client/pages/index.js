4bcc848fd98a938474479f36486a3e9cc89764df
rusty <dalegribble@riseup.net>
2021-10-30 20:48:51 -0700

    add syn to team section on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -243,14 +243,14 @@ export default function Home() {
 					<h1 class="text-2xl font-medium title-font mb-4 text-gray-900">OUR TEAM</h1>
 					<p class="lg:w-2/3 mx-auto leading-relaxed text-base">World-class development meets crypto-currency expertise, our team is a unification of skill and passion. </p>
 					</div>
-					<div class="flex flex-wrap -m-4">
+					<div class="flex flex-wrap -m-4 justify-center">
 					<div class="p-4 lg:w-1/4 md:w-1/2">
 						<div class="h-full flex flex-col items-center text-center">
 						<img alt="team" class="flex-shrink-0 rounded-lg w-full h-128 object-cover object-center mb-4" src="/rusty.png" />
 						<div class="w-full">
 							<h2 class="title-font font-medium text-lg text-gray-900">rusty</h2>
 							<h3 class="text-gray-500 mb-3">Lead Developer</h3>
-							<p class="mb-4">Read "Smart Contracts for Dummies" and decided to develop his own bleeding-edge tools. Owns a DigitalGangster.</p>
+							<p class="mb-4">Read "Smart Contracts for Dummies" and decided to develop his own bleeding-edge tools.</p>
 							<span class="inline-flex">
 							<a class="ml-2 text-gray-500">
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -268,34 +268,12 @@ export default function Home() {
 					</div>
 					<div class="p-4 lg:w-1/4 md:w-1/2">
 						<div class="h-full flex flex-col items-center text-center">
-						<img alt="team" class="flex-shrink-0 rounded-lg w-full h-128 object-cover object-center mb-4" src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/4e/4ed0b412a943b60d4dc5d2330c33d3279ebd85a1_full.jpg" />
+						<img alt="team" class="flex-shrink-0 rounded-lg w-full h-128 object-cover object-center mb-4" src="/syn.png" />
 						<div class="w-full">
-							<h2 class="title-font font-medium text-lg text-gray-900">razor</h2>
+							<h2 class="title-font font-medium text-lg text-gray-900">Syn</h2>
 							<h3 class="text-gray-500 mb-3">Developer</h3>
-							<p class="mb-4">Who knew Germany decided to colonize IDA Pro? An expert in reverse-engineering, razor started in the game-hacking scene.</p>
+							<p class="mb-4">PLACEHOLDER</p>
 							<span class="inline-flex">
-							</span>
-						</div>
-						</div>
-					</div>
-					<div class="p-4 lg:w-1/4 md:w-1/2">
-						<div class="h-full flex flex-col items-center text-center">
-						<img alt="team" class="flex-shrink-0 rounded-lg w-full h-128 object-cover object-center mb-4" src="https://lh3.googleusercontent.com/N_ZKOnP-tOgG8cfCYcbg7fk5xFsX1xQ53L0J5hqS6NGaLCfFgEi44Ws0L3nHbCQlb8V7jvW99P5ehOgT7GdfrttOCMqLxo_f5tdbRg=w600" />
-						<div class="w-full">
-							<h2 class="title-font font-medium text-lg text-gray-900">darku</h2>
-							<h3 class="text-gray-500 mb-3">Market Analyst</h3>
-							<p class="mb-4">Reincarnated stock-broker, first man to walk in NFT-space, international man of mystery, files his own taxes.</p>
-							<span class="inline-flex">
-							<a class="ml-2 text-gray-500">
-								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-								<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-								</svg>
-							</a>
-							<a class="ml-2 text-gray-500">
-								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-								<path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-								</svg>
-							</a>
 							</span>
 						</div>
 						</div>
